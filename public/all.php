d75568b43ce07656b5b6be090ff7c1d5b9c44b8b
root <root@sud0.me>
2019-04-18 04:30:46 +0000

    Re-order field position

--- public/all.php
+++ public/all.php
@@ -1,3 +1,16 @@
 <?php
 require_once 'wcis-ajax.php';
 require_once 'wcis-frontend.php';
+
+/**
+ * Re-order field checkout
+ * Move field City after state
+ */
+add_filter( 'woocommerce_default_address_fields', 'shipping_indo_reorder_checkout_fields' );
+  
+function shipping_indo_reorder_checkout_fields( $fields ) {
+  $fields['state']['priority'] = 70;
+  $fields['city']['priority'] = 80;
+  return $fields;
+}
+
